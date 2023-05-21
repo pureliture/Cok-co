eee92ec6866776547eb70ce7d8a20a030797b528
LeeJuhae <leejuhae821@gmail.com>
2023-05-21 16:01:54 +0900

    [feat] 뒤로가기 추가

--- src/components/GoBackBar.js
+++ src/components/GoBackBar.js
@@ -1,9 +1,16 @@
 import { React } from 'react';
 import './styles/GoBackBar.css';
+import { useNavigate } from 'react-router-dom';
 
 const GoBackBar = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="go_back_bar">
+    <div className="go_back_bar" onClick={handleClick}>
       <img className="left_arrow" src="/left_arrow.png"></img>
     </div>
   );
